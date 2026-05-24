307e8b1035e9c430f81c315dd3f3b26d1d64cccb
ths666 <2451663190@qq.com>
2026-05-24 17:51:03 +0800

    feat: configure signed Windows updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,13 @@ mod commands;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build())?;
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![commands::backend_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
